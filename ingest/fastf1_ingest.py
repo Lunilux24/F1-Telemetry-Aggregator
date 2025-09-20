c73f96e803249aa0bc305c4bc0f0f6bee1e9c2c5
Bassim Beshry <70615080+Lunilux24@users.noreply.github.com>
2025-09-19 20:20:04 -0600

    FIX: Pulling Result table from fastf1 now

--- ingest/fastf1_ingest.py
+++ ingest/fastf1_ingest.py
@@ -66,7 +66,8 @@ def fetch_fastf1():
     # TELEMETRY DATA (Can Modify)
     data = {
         "laps": json.loads(session.laps.to_json(orient="records")),
-        "weather": json.loads(session.weather_data.to_json(orient="records"))
+        "weather": json.loads(session.weather_data.to_json(orient="records")),
+        "results": json.loads(session.results.to_json(orient="records"))
     }
     return json.dumps(data).encode('utf-8')
 
